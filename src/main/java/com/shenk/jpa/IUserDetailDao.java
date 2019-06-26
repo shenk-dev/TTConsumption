5756de7aa09fd0e1a3fcb8a82bab439f5ad6c31c
shenk <shenk@tfinfo.cn>
2019-06-26 10:42:00 +0800

    delete jpa config

--- src/main/java/com/shenk/jpa/IUserDetailDao.java
+++
@@ -1,7 +0,0 @@
-package com.shenk.jpa;
-
-import com.shenk.entity.UserDetail;
-import org.springframework.data.repository.CrudRepository;
-
-public interface IUserDetailDao extends CrudRepository<UserDetail, String> {
-}
